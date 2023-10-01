460e2eccd584285ec688084a59102fbe2d0a3a1e
vishnuvishwakarma <vv083150@gmail.com>
2023-10-01 20:15:11 +0530

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { $query, $update } from 'azle';
+import { $query, $update,Result } from 'azle';
 
 // This is a global variable that is stored on the heap
 let message: string = '';
@@ -6,13 +6,27 @@ let message: string = '';
 // Query calls complete quickly because they do not go through consensus
 $query;
 export function getMessage(): string {
-    return message;
+    try {
+        return message;
+    } catch (error) {
+        console.error(error);
+        return 'Null';
+    }
 }
 
 // Update calls take a few seconds to complete
 // This is because they persist state changes and go through consensus
 $update;
-export function setMessage(newMessage: string): void {
-    message = newMessage; // This change will be persisted
+export function setMessage(newMessage: string): boolean {
+    if (!newMessage) {
+        throw Result.Err ("Invalid message!")
+    }
+    try {
+        message = newMessage; // This change will be persisted
+        return true;
+    } catch (error) {
+        // Handle the error here, e.g. log it or display a user-friendly message
+        return false;
+    }
 }
 
